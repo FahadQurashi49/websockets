d1fe6864d71eda2d8f4f2a9dc381b6e5bfb0a63a
FahadQurashi49 <fahad.ahmad049@gmail.com>
2017-10-17 00:24:01 +0500

    show who is typing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var socket = require("socket.io");
 
 // App setup
 var app = express();
-var server = app.listen(4000, function() {
+var server = app.listen(4000, function () {
     console.log("listening to request on port 4000");
 });
 
@@ -12,9 +12,18 @@ app.use(express.static("public"));
 
 var io = socket(server);
 
+
 io.on("connection", function name(socket) {
-   console.log("connection established!", socket.id); 
-   socket.on('chat', function (data) {
-       io.sockets.emit('chat', data); 
-   });
+    console.log("connection established!", socket.id);
+
+    // handle chat event
+    socket.on('chat', function (data) {
+        io.sockets.emit('chat', data);
+    });
+
+    // handle typing event
+    socket.on('typing', function (data) {
+        socket.broadcast.emit('typing', data);
+    });
+
 });
